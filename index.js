6e52b75d62257fbc706fd426fbee325d9544d73c
disco0 <disco0@users.noreply.github.com>
2020-11-20 18:44:44 -0800

    ts rewrite

--- index.js
+++ index.js
@@ -1,38 +1,63 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const chalk_1 = __importDefault(require("chalk"));
+const userscriptMetaBlockRegex = /\B(?<metablock>\/\/ ==UserScript==\r?\n(?<metas>[\S\s]*?)\r?\n\/\/ ==\/UserScript==)(?<code>[\S\s]*)/;
+const defaultReturnValue = undefined;
 /**
  * Userscript format:
  * http://wiki.greasespot.net/Metadata_Block
  */
-
-module.exports = function extractMetablock (userscriptText) {
-  try {
-    var blocksReg = /\B(\/\/ ==UserScript==\r?\n([\S\s]*?)\r?\n\/\/ ==\/UserScript==)([\S\s]*)/
-    var blocks = userscriptText.match(blocksReg)
-
-    if (!blocks) {
-      return null
+function extractMetablock(userscriptText) {
+    try {
+        const blocks = userscriptText.match(userscriptMetaBlockRegex);
+        console.dir(blocks);
+        if (!blocks) {
+            return defaultReturnValue;
+        }
+        const { metablock, metas, code } = blocks.groups;
+        //#region Debug
+        console.log(chalk_1.default.magenta `metablock`);
+        console.dir(metablock);
+        console.log(chalk_1.default.magenta `metas`);
+        console.dir(metas);
+        console.log(chalk_1.default.magenta `code`);
+        console.dir(code);
+        //#endregion Debug
+        const meta = {};
+        const metaArray = metas.split(/\n/);
+        for (const metaItem of metaArray) {
+            const parts = metaItem.match(/@([\w-]+)\s+(.+?)\s*$/);
+            if (parts) {
+                // if(!(parts[1] in meta))
+                //     meta[parts[1]] = [] as string[];
+                meta[parts[1]] = meta[parts[1]] || [];
+                meta[parts[1]].push(parts[2]);
+            }
+            else {
+                console.error(chalk_1.default.red `Failed to parse meta from line: ${chalk_1.default.bold.red(metaItem)}`);
+            }
+        }
+        // metaArray.forEach(function(m)
+        // {
+        //     var parts = m.match(/@([\w-]+)\s+(.+)/)
+        //     if(parts)
+        //     {
+        //         meta[parts[1]] = meta[parts[1]] || []
+        //         meta[parts[1]].push(parts[2])
+        //     }
+        // })
+        return {
+            meta,
+            metablock,
+            content: code
+        };
     }
-
-    var metablock = blocks[1]
-    var metas = blocks[2]
-    var code = blocks[3]
-
-    var meta = {}
-    var metaArray = metas.split('\n')
-    metaArray.forEach(function (m) {
-      var parts = m.match(/@([\w-]+)\s+(.+)/)
-      if (parts) {
-        meta[parts[1]] = meta[parts[1]] || []
-        meta[parts[1]].push(parts[2])
-      }
-    })
-
-    return {
-      meta: meta,
-      metablock: metablock,
-      content: code
+    catch (e) {
+        if (console)
+            console.error(e);
+        return defaultReturnValue;
     }
-  } catch(e) {
-    if (console) console.error(e)
-    return null
-  }
 }
+module.exports = extractMetablock;
--- test/parser.spec.js
+++ test/parser.spec.js
@@ -1,32 +1,35 @@
-const fs = require('fs')
-const tape = require('tape')
-const usp = require('../')
-
-tape.test('export a parser function', function (t) {
-  t.equal(typeof usp, 'function', 'require("userscript") should be a function')
-  t.end()
-})
-
-tape.test('parse a valid userscript -> { meta, metablock, content }', function (t) {
-  const userscript = fs.readFileSync('./test/valid-userscript.user.js', 'utf8')
-  const result = usp(userscript)
-
-  t.deepEqual(result.meta, {
-    description: [ 'This script even does the laundry!', ],
-    downloadURL: [ 'https://www.example.com/myscript.user.js' ],
-    name: [ 'Awesome Script' ],
-    homepageURL: [ 'https://github.com/gantt/downloadyoutube' ],
-    author: [ 'Gantt' ],
-    version: [ '1.8.3' ],
-    date: [ '2015-05-17' ],
-    include: [ 'https://www.youtube.com/*' ],
-    exclude: [ 'https://www.youtube.com/embed/*' ],
-    match: [ 'https://www.youtube.com/*' ],
-    grant: [ 'GM_xmlhttpRequest', 'GM_getValue', 'GM_setValue' ],
-    'run-at': [ 'document-end' ],
-    license: [ 'MIT License' ]
-  })
-  t.equal(result.metablock.trim(), `// ==UserScript==
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const tape = require("tape");
+const __1 = __importDefault(require("../"));
+const defaultReturnValue = undefined;
+tape('export a parser function', function (test) {
+    test.equal(typeof __1.default, 'function', 'require("userscript") should be a function');
+    test.end();
+});
+tape('parse a valid userscript -> { meta, metablock, content }', function (test) {
+    const userscript = fs_1.default.readFileSync('./test/valid-userscript.user.js', 'utf8');
+    const result = __1.default(userscript);
+    test.deepEqual(result === null || result === void 0 ? void 0 : result.meta, {
+        description: ['This script even does the laundry!',],
+        downloadURL: ['https://www.example.com/myscript.user.js'],
+        name: ['Awesome Script'],
+        homepageURL: ['https://github.com/gantt/downloadyoutube'],
+        author: ['Gantt'],
+        version: ['1.8.3'],
+        date: ['2015-05-17'],
+        include: ['https://www.youtube.com/*'],
+        exclude: ['https://www.youtube.com/embed/*'],
+        match: ['https://www.youtube.com/*'],
+        grant: ['GM_xmlhttpRequest', 'GM_getValue', 'GM_setValue'],
+        'run-at': ['document-end'],
+        license: ['MIT License']
+    });
+    test.equal(result === null || result === void 0 ? void 0 : result.metablock.trim(), `// ==UserScript==
 // @name        Awesome Script
 // @description This script even does the laundry!
 // @downloadURL https://www.example.com/myscript.user.js
@@ -42,12 +45,11 @@ tape.test('parse a valid userscript -> { meta, metablock, content }', function (
 // @grant       GM_setValue
 // @run-at      document-end
 // @license     MIT License
-// ==/UserScript==`)
-  t.equal(result.content.trim(), 'var whoami = "USERSCRIPT"')
-  t.end()
-})
-
-tape.test('parse an invalid userscript -> null', function (t) {
-  t.equal(usp(''), null)
-  t.end()
-})
+// ==/UserScript==`);
+    test.equal(result === null || result === void 0 ? void 0 : result.content.trim(), 'var whoami = "USERSCRIPT"');
+    test.end();
+});
+tape('parse an invalid userscript -> null', function (t) {
+    t.equal(__1.default(''), defaultReturnValue);
+    t.end();
+});
